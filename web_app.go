8b6c72ea4b7b1eeef50b6be82d408ab71f9d1b93
Syed Ahmed Jamil <ahmedjamilcs@gmail.com>
2023-05-30 16:14:16 +0500

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -7,7 +7,7 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r * http.Request) {
         fmt.Fprint(w, "Hello!")
     })
-    err: = http.ListenAndServe("0.0.0.0:80", nil)
+    err := http.ListenAndServe("0.0.0.0:80", nil)
     if err != nil {
         panic(err)
     }
